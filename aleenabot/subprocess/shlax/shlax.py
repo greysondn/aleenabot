11f30261cacaa91e6c20f48048cfb8e950c0e85b
J. 'Dorian Greyson' L <greysondn@gmail.com>
2022-12-07 18:40:25 -0500

    Enjoy your color commentary

--- aleenabot/subprocess/shlax/shlax.py
+++ aleenabot/subprocess/shlax/shlax.py
@@ -5,17 +5,21 @@ import shlex
 import sys
 
 class SubprocessProtocol(asyncio.SubprocessProtocol):
+    # this gets handed the child ShlaxProcess
     def __init__(self, proc):
         self.proc = proc
         self.output = bytearray()
 
     def pipe_data_received(self, fd, data):
         if fd == 1:
+            # helper function ShlaxProcess.stdout
             self.proc.stdout(data)
         elif fd == 2:
+            # helper function ShlaxProcess.stderr
             self.proc.stderr(data)
 
     def process_exited(self):
+        # future of proc object
         self.proc.exit_future.set_result(True)
 
 class Subprocess:
@@ -34,18 +38,30 @@ class Subprocess:
         flush=None,
     ):
         if len(args) == 1 and ' ' in args[0]:
+            # looks like a default so it can't crash to death
             args = ['sh', '-euc', args[0]]
 
+        # okay, has to be app plus its args, right?
         self.args = args
+        # rewrite - default plus type opt-in
         self.quiet = quiet if quiet is not None else False
+        # probably remove
         self.prefix = prefix
+        # probably remove
         self.write = write or sys.stdout.buffer.write
+        # probably remove
         self.flush = flush or sys.stdout.flush
+        # not so bad
         self.started = False
+        # ew, only on joins
         self.waited = False
+        # ew
         self.out_raw = bytearray()
+        # ew
         self.err_raw = bytearray()
 
+        # even in the original this is non-obvious
+        # will probably trash it
         self.regexps = dict()
         if regexps:
             for search, replace in regexps.items():
@@ -56,6 +72,10 @@ class Subprocess:
 
     async def start(self, wait=True):
         if not self.quiet:
+            # probably attempts to notify it was started, or at least called to
+            # start. Some of this was butchered out.
+            #
+            # FIXME: Fix or cut the rest of the way out
             self.output(
                 b'+ '
                 + shlex.join([
@@ -69,6 +89,7 @@ class Subprocess:
         # low-level APIs.
         loop = asyncio.get_running_loop()
 
+        # neat but probably not meaningful
         self.exit_future = asyncio.Future(loop=loop)
 
         # Create the subprocess controlled by DateProtocol;
@@ -76,10 +97,16 @@ class Subprocess:
         self.transport, self.protocol = await loop.subprocess_exec(
             lambda: SubprocessProtocol(self),
             *self.args,
+            # FIXME: I will want stdin to be a pipe, too.
             stdin=None,
         )
+        # note on above fixme:
+        # proc.stdin.write(input)
+        # await process.stdin.drain() # flush
+        
         self.started = True
 
+    # basically a join innit?
     async def wait(self, *args, **kwargs):
         if not self.started:
             await self.start()
@@ -96,11 +123,13 @@ class Subprocess:
 
         return self
 
+    # FIXME: trash, replace with to-buffer handling
     def stdout(self, data):
         self.out_raw.extend(data)
         if not self.quiet:
             self.output(data)
 
+    # FIXME: trash, replace with to-buffer handling
     def stderr(self, data):
         self.err_raw.extend(data)
         if not self.quiet:
@@ -118,6 +147,7 @@ class Subprocess:
     def rc(self):
         return self.transport.get_returncode()
 
+    # FIXME: trash, maybe entirely
     def output(self, data, highlight=True, flush=True):
         for line in data.strip().split(b'\n'):
             line = [self.highlight(line) if highlight else line]
