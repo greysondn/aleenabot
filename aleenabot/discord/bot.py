fb28a2fade4b653eaa0a05951206a9202f535bec
J. 'Dorian Greyson' L <greysondn@gmail.com>
2022-12-15 20:23:05 -0500

    break everything by injecting it into a class

--- aleenabot/discord/bot.py
+++ aleenabot/discord/bot.py
@@ -4,63 +4,65 @@ import argparse
 import discord
 import logging
 
-# figuring out what intents I need is like
-# https://discordpy.readthedocs.io/en/stable/intents.html
-# provides checklist
+class AleenaBotDiscord():
+    
+    # figuring out what intents I need is like
+    # https://discordpy.readthedocs.io/en/stable/intents.html
+    # provides checklist
 
-intents = discord.Intents.default()
-# auto_moderation
-# auto_moderation_configuration
-# auto_moderation_execution
-# bans
-# dm_messages
-# dm_reactions
-# dm_typing
-# emojis
-# emojis_and_stickers
-# guild_messages
-# guild_reactions
-# guild_scheduled_events
-# guild_typing
-# guilds
-# integrations
-# invites
-# members
-intents.message_content = True
-# messages
-# presences
-# reactions
-# typing
-# value
-# voice_states
-# webhooks
+    intents = discord.Intents.default()
+    # auto_moderation
+    # auto_moderation_configuration
+    # auto_moderation_execution
+    # bans
+    # dm_messages
+    # dm_reactions
+    # dm_typing
+    # emojis
+    # emojis_and_stickers
+    # guild_messages
+    # guild_reactions
+    # guild_scheduled_events
+    # guild_typing
+    # guilds
+    # integrations
+    # invites
+    # members
+    intents.message_content = True
+    # messages
+    # presences
+    # reactions
+    # typing
+    # value
+    # voice_states
+    # webhooks
 
-client = discord.Client(intents=intents)
+    client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+    @client.event
+    async def on_ready():
+        print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-
-logHandler = logging.FileHandler(
-                                    filename="discord.log", 
-                                    encoding="utf-8",
-                                    mode="a"
-                                )
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
 
 
-client.run(
-                'your token here',
-                log_handler=logHandler
-          )
+    logHandler = logging.FileHandler(
+                                        filename="discord.log", 
+                                        encoding="utf-8",
+                                        mode="a"
+                                    )
+
+
+    client.run(
+                    'your token here',
+                    log_handler=logHandler
+            )
 
 def main():
     parser = argparse.ArgumentParser(
