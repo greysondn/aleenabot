301ae949be8f506f5c497003545b15990f636c71
J. 'Dorian Greyson' L <greysondn@gmail.com>
2022-12-15 19:04:42 -0500

    Better discord template

--- aleenabot/discord/bot.py
+++ aleenabot/discord/bot.py
@@ -2,15 +2,21 @@
 
 import discord
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run('my token goes here')
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('your token here')
