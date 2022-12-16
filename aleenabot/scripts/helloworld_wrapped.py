f02d1b844a735b393ad8f85d1bee89d7c91c8157
J. 'Dorian Greyson' L <greysondn@gmail.com>
2022-12-16 04:13:15 -0500

    rewrite test wrapprt

--- aleenabot/scripts/helloworld_wrapped.py
+++ aleenabot/scripts/helloworld_wrapped.py
@@ -1,37 +1,15 @@
 import aleenabot.subprocess.helpers as hlp
 import asyncio as aio
-    
-
-class Main(hlp.Manager):
-    def __init__(self):
-        # call parent constructor
-        super().__init__("main")
-    
-    def checkDead(self):
-        return False
-        
-    async def main(self):
-        # create main program?
-        hwProc:hlp.ProcessWrapper = hlp.ProcessWrapper(
-            name = "Hello World!",
-            command = "python helloworld.py"
-        )
-        
-        # add to children
-        await self.addChild(hwProc)
-        
-        # just want to sanity check that output
-        await hwProc.message(message = "Message wiring is good.")
-        
-        # okay um ignite it?
-        await hwProc.main()
-        
-        # and now I wait for that to die?
-        await hlp.waitUntilFunctionTrue(self.checkDead)
 
 async def aioMain():
-    m = Main()
-    await m.main()
+    main = hlp.Manager()
+    proc = hlp.SubprocessWrapper(
+            "python helloworld.py",
+            name="Hello, World!",
+            tg = main.tg
+        )
+    await proc.main()
+    await main.start()
         
 if (__name__ == "__main__"):
     aio.run(aioMain())
