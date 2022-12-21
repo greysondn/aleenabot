1e902bed5111f4b3f6b5a25c027f389a0511e4b1
J. 'Dorian Greyson' L <greysondn@gmail.com>
2022-12-21 06:03:35 -0500

    Add minecraft 1.16.5 to db

--- aleenabot/scripts/mc_scratchpad.py
+++ aleenabot/scripts/mc_scratchpad.py
@@ -12,11 +12,28 @@ def createDB():
     migrator.run_all("0000", "0001", [MGT.CORE, MGT.MINECRAFT])
 
 # ------------------------------------------------------------------------------
+
+def dbAddVanillaMinecraftOneSixteenFive():
+    mod, created = db.MCMod.get_or_create(name = "Minecraft")
+    version, created = db.MCMinecraftVersion.get_or_create(version="1.16.5", nickname="The Nether Update")
+    loader, created = db.MCModLoader.get_or_create(name = "Minecraft (Vanilla)")
+    modVersion, created = db.MCModVersion.get_or_create(
+                                                            mod = mod,
+                                                            minecraftVersion = version,
+                                                            modLoader = loader,
+                                                            version = "1.16.5",
+                                                            sha3Checksum  = "skip",
+                                                            shakeChecksum = "skip",
+                                                            filename    = "skip"
+                                                       )
+
+# ------------------------------------------------------------------------------
 # main
 # ------------------------------------------------------------------------------
 
 def main():
-    createDB()
-
+    db.initDB()
+    dbAddVanillaMinecraftOneSixteenFive()
+    
 if __name__ == "__main__":
     main()
