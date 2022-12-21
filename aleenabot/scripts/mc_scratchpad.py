22dec3448140f249aacfeed0e348a9e1e9c6aeb8
J. 'Dorian Greyson' L <greysondn@gmail.com>
2022-12-21 11:40:44 -0500

    Add Vanilla items from CSV files

--- aleenabot/scripts/mc_scratchpad.py
+++ aleenabot/scripts/mc_scratchpad.py
@@ -5,7 +5,7 @@ from aleenabot.database.migration import initMigrator
 from aleenabot.database.migration import dbMigrationManager as migrator
 from aleenabot.database.migration import DBMigrationType as MGT
 import aleenabot.database.database as db
-
+import pandas as pd
 
 def createDB():
     initMigrator()
@@ -28,12 +28,75 @@ def dbAddVanillaMinecraftOneSixteenFive():
                                                        )
 
 # ------------------------------------------------------------------------------
+
+def dbAddItemsFromCSV():
+    csv = pd.read_csv(".\\__data\\vanilla\\items.csv")
+    
+    mc_mod         = db.MCMod.get(name = "Minecraft")
+    mc_modVersion  = db.MCModVersion.get(mod = mc_mod, version = "1.16.5")
+    
+    for i in range(len(csv)):
+        # get line
+        line = csv.iloc[i]
+    
+        # should be an item, so let's just do it in order.
+        i_isblockitem        = False
+        if (line["Is BlockItem"] == "+"):
+            i_isblockitem    = True
+        
+        # and we can just create it from there, with
+        # awful defaults for many items, I guess
+        item, created = db.MCItem.get_or_create(
+                name                    = line["Display Name"],
+                registryName            = line["ID"],
+                lore                    = line["Tooltip"],
+                nbt                     = line["Tag"],
+                source                  = mc_mod,
+                firstSourceVersion      = mc_modVersion,
+                lastSourceVersion       = mc_modVersion,
+                material                = None,
+                slot                    = None,
+                archetype               = None,
+                translationKey          = line["Translation Key"],
+                isBlockitem             = i_isblockitem,
+                attackDamage            = 0.0,
+                attackDamageModifier    = 0.0,
+                attackSpeed             = 0.0,
+                attackSpeedModifer      = 0.0,
+                burnTime                = float(line["Burn Time"]),
+                canEatWhileFull         = False,
+                damageReduceAmount      = 0.0,
+                durability              = 0.0,
+                efficiency              = 0.0,
+                enchantability          = float(line["Enchantability"]),
+                harvestLevel            = 0.0,
+                healing                 = 0.0,
+                isFastEating            = 0.0,
+                isMeat                  = False,
+                inventoryModelName      = 0.0,
+                maxDamage               = float(line["Max Damage"]),
+                maxStackSize            = int(line["Max Stack Size"]),
+                saturation              = 0.0,
+                toughness               = 0.0,
+                useDuration             = 0.0,
+        )
+
+        # and now tags
+        if (str(line["Tags"]) != "nan"):
+            swp_tags = line["Tags"].split()
+            
+            for tag in swp_tags:
+                if (tag.strip() != ""):
+                    i_tag, created = db.MCItemTag.get_or_create(registryName = tag.strip())
+                    
+                    db.MCItems_to_MCTags.get_or_create(item = item, tag = i_tag)
+# ------------------------------------------------------------------------------
 # main
 # ------------------------------------------------------------------------------
 
 def main():
     db.initDB()
-    dbAddVanillaMinecraftOneSixteenFive()
+    dbAddItemsFromCSV()
     
 if __name__ == "__main__":
     main()
