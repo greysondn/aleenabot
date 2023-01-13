76b237af4445e6a745b51cdbabfbabb66acdbfd4
J. 'Dorian Greyson' L <greysondn@gmail.com>
2023-01-13 18:21:38 -0500

    Begin work on getting echo going

---
+++ aleenabot/scripts/echo_wrapped.py
@@ -0,0 +1,22 @@
+import aleenabot.subprocess.wrappers as hlp
+import asyncio as aio
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+async def aioMain():
+    main = hlp.Manager()
+    proc = hlp.SubprocessWrapper(
+        'python',
+        "C:\\Users\\Dorian Greyson\\git\\aleenabot\\aleenabot\\scripts\\echo.py",
+        name = "Echo",
+        tg = main.tg
+    )
+    
+    await main.addChild(proc)
+    
+    await main.start()
+    await main.wait()
+    
+if (__name__ == "__main__"):
+    aio.run(aioMain())
