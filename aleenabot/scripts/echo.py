b548caebc18d92550f7b763d0469cfa31d667d5d
J. 'Dorian Greyson' L <greysondn@gmail.com>
2023-01-13 19:55:04 -0500

    Tell user when we load and exit

--- aleenabot/scripts/echo.py
+++ aleenabot/scripts/echo.py
@@ -2,12 +2,14 @@
 # windows doesn't seem to have the traditional echo command.
 
 def main():
+    print("Loaded!")
     carryOn = True
     while carryOn:
         userInput = input("")
         if (userInput == "exit"):
             carryOn = False
         print(userInput)
+    print("Exited!")
 
 if ("__main__" == __name__):
     main()
